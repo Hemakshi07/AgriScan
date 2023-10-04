3f505c80019a39ca8fbd48d95deb52448c64a301
Rajvi Raval <ravalrajvi55555@gmail.com>
2023-10-04 16:11:39 -0400

    Dashboard setup

--- agriScanApp/urls.py
+++ agriScanApp/urls.py
@@ -2,7 +2,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.contrib.auth import views as auth_views
+app_name = 'agriScanApp'
 
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
+    # path('login/', views.LoginView, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+   
+
 ]
--- agriScanApp/views.py
+++ agriScanApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import DashboardItem
 # Create your views here.
 from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello, Django!")
+
+
+def dashboard(request):
+    # items = DashboardItem.objects.all()
+    return render(request, 'agriScanApp/dashboard.html')
